f752dd61cd819a66bbcb2b824e35a2e63aaabb25
RaFl91 <Terrell.Flemings@gmail.com>
2018-10-22 14:47:07 -0400

    revision: Brunch schema, creation: user views/ and layout + user controller

--- models/BRUNCH.js
+++ models/BRUNCH.js
@@ -1,19 +1,14 @@
 // requirements: require db/connection as 'mongoose'
 const mongoose = require('../controllers/brunch')
-// const Donut = new mongoose.Schema
 
 
-// create your donut schema:
 
 const Brunch = new mongoose.Schema({
-  name: String,
+  title: String,
   description: String,
-  img: String,
-  price: Number,
-  qty: Number
-});
-// export the donut model with module.exports
+  instructions: String,
+  ingredients: Number
+})
 
 module.exports = mongoose.model('Brunch', Brunch)
-// this is the action of Mongoose saying "create a collection called donuts and make it equal to what the Schema says"
-// "created donut schema"
+// this is the action of Mongoose saying "create a collection called brunch and make it equal to what the Schema says"

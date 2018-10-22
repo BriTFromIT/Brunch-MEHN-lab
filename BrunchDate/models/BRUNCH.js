f52cebc3d6f29229a855e290f5e212a00ca78189
BrittFromIT <Brittanylewis32@gmail.com>
2018-10-22 18:31:50 -0400

    bhbhjbhk

--- BrunchDate/models/BRUNCH.js
+++ BrunchDate/models/BRUNCH.js
@@ -1,14 +1,16 @@
-// requirements: require db/connection as 'mongoose'
-const mongoose = require('../controllers/brunch')
+// requirements: require db/connection as ‘mongoose’
+const mongoose = require(‘../db/connections’)
+const Schema =  mongoose.Schema
 
-
-
-const Brunch = new mongoose.Schema({
-  title: String,
-  description: String,
-  instructions: String,
-  ingredients: Number
+const Brunch = Schema ({
+ content: {
+ title: String,
+ description: String,
+ instructions: String,
+ ingredients: Number
+ }
 })
 
-module.exports = mongoose.model('Brunch', Brunch)
+
+module.exports = mongoose.model(‘Brunch’, Brunch)
 // this is the action of Mongoose saying "create a collection called brunch and make it equal to what the Schema says"

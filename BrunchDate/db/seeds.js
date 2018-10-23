23da358c8727f694cc3c2e46d1968f8b19c9658a
RaFl91 <Terrell.Flemings@gmail.com>
2018-10-23 09:00:25 -0400

    fixed seeds file

--- BrunchDate/db/seeds.js
+++ BrunchDate/db/seeds.js
@@ -1,59 +1,70 @@
 let Brunch = require('../models/BRUNCH')
 
-let newBrunch = [
-    {
-        title: 'Pepper’d Potato Frittata',
-        description: 'Like omelets and quiches, a nice frittata recipe is also a popular dish for breakfast and brunch. For the most part, Frittata recipes are super-versatile and super simple. From cheese lovers, to meat lovers, frittatas are popular with any crowd. This creamy, cheesy skillet-cooked pepper’d potato frittata gets a nice shot of color and flavor from green pepper rings and bits of chopped bacon.',
-        img: 'http://assets.kraftfoods.com/recipe_images/opendeploy/113397_640x428.jpg',
-        instructions: {
-            ['Heat oven to 350°F',
-                'Cook bacon in medium nonstick ovenproof skillet 4 min',
-                'drain. Add potatoes and onions',
-                'cook 7 min. or until crisp-tender',
-                'stirring frequently',
-                'Stir in eggs. Remove from heat. Top with cheese and peppers; cover.',
-                'Bake 20 to 25 min. or until center is set'
-            ]
-        },
-    ingredients: {
-            [
-                '6 slices of chopped bacon',
-                '1 cup shredded potatoes',
-                '1/4 cup chopped onions', 
-                '6 eggs, beaten', 
-                '1 cup Shredded Cheddar Pepper Jack Cheese', 
-                '1/2 cup green pepper rings'
-            ]
-    }
-  }
- 
+// let newBrunch = [{
+//     title: 'Pepper’d Potato Frittata',
+//     description: 'Like omelets and quiches, a nice frittata recipe is also a popular dish for breakfast and brunch. For the most part, Frittata recipes are super-versatile and super simple. From cheese lovers, to meat lovers, frittatas are popular with any crowd. This creamy, cheesy skillet-cooked pepper’d potato frittata gets a nice shot of color and flavor from green pepper rings and bits of chopped bacon.',
+//     img: 'http://assets.kraftfoods.com/recipe_images/opendeploy/113397_640x428.jpg',
+//     instructions:{
+//         ['Heat oven to 350°F',
+//             'Cook bacon in medium nonstick ovenproof skillet 4 min',
+//             'drain. Add potatoes and onions',
+//             'cook 7 min. or until crisp-tender',
+//             'stirring frequently',
+//             'Stir in eggs. Remove from heat. Top with cheese and peppers; cover.',
+//             'Bake 20 to 25 min. or until center is set'
+//         ]
+//     },
+//     ingredients: {
+//         [
+//             '6 slices of chopped bacon',
+//             '1 cup shredded potatoes',
+//             '1/4 cup chopped onions',
+//             '6 eggs, beaten',
+//             '1 cup Shredded Cheddar Pepper Jack Cheese',
+//             '1/2 cup green pepper rings'
+//         ]
+//     }
+// }]
 
-//   {
-//     name: 'Plain Ole Donut',
+let newBrunch = [{
+    title: `Pepper'd Potato Frittata`,
+    description: `Like omelets and quiches, a nice frittata recipe is also a popular dish for breakfast and brunch.
+    For the most part, frittata recipes are super versatile and really simple. From cheese lovers to meat lovers, frittatas are popular with any crowd.
+    This creamy, cheesy skillet-cooked pepper'd potato frittata gets a nice shot of color and flavor from green pepper rings and bits of chopped bacon.`,
+    img: `http://assets.kraftfoods.com/recipe_images/openedeploy/113397_640x428.jpg`,
+    instructions: `Heat oven to 350° F.  Cook bacon in medium non-stick, ovenproof skillet for 4 mins.  Drain, then add potatoes and onions.  
+    Cook for 7 mins, or ntil crisp and tender.  Stirring frequently, add in eggs.  Remove from heat, then top with cheese and peppers. Cover skillet.
+    Bake fr 20 - 25 mins, or until center is set.`,
+    ingredients: `6 slices of bacon: chopped, 1 cup shredded potatoes, 1/4 cup chopped onions, 6 eggs: beaten, 1 cup shredded Cheddar and Pepper Jack
+    cheese, 1/2 cup green pepper rings`
+    
+}]
+
+
+//   {    name: 'Plain Ole Donut',
 //     description: 'Plain donut for plain people.',
 //     img: 'https://grandmastersensei.files.wordpress.com/2009/06/donut.jpg',
 //   price: 25,
 //     qty: 15
-        
-            
+
+
 //                 name: 'Chocolate',
 //                     description: 'Chocolate Donut nom nom nom',
 //   img:
 //     'https://deerfieldsbakery.com/images/items/Other/Pastries-Donuts-Chocolate-Iced-Glazed_MD.jpg',
 //     price: 7000,
 //         qty: 1
-            
-    
+
+
 //         name: 'Donut Holes',
 //             description: 'For the snackers.',
 //   img:
 //     'http://sweets.seriouseats.com/images/20110313-142295-Dough-DonutHoles.jpg',
 //     price: 10,
 //         qty: 23
-            
-    
-        
-            Brunch.create(newBrunch).then(brunches => {
-console.log('Saved Brunches', brunches)
-})
 
+
+
+Brunch.create(newBrunch).then(brunches => {
+    console.log('Saved Brunches', brunches)
+})
--- BrunchDate/models/BRUNCH.js
+++ BrunchDate/models/BRUNCH.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-// requirements: require db/connection as 'mongoose'
-const mongoose = require('../db/connections')
-const Schema = mongoose.Schema
-
-const Brunch = new Schema({
-  title: String,
-  description: String,
-  instructions: String,
-  ingredients: String  
-});
-=======
 // requirements: require db/connection as ‘mongoose’
-const mongoose = require(‘../db/connections’)
+const mongoose = require(`../db/connections`)
 const Schema =  mongoose.Schema
 
 const Brunch = Schema ({
@@ -22,8 +10,7 @@ const Brunch = Schema ({
  ingredients: String
  }
 })
->>>>>>> f52cebc3d6f29229a855e290f5e212a00ca78189
 
 
-module.exports = mongoose.model(‘Brunch’, Brunch)
+module.exports = mongoose.model(`Brunch`, Brunch)
 // this is the action of Mongoose saying "create a collection called brunch and make it equal to what the Schema says"
